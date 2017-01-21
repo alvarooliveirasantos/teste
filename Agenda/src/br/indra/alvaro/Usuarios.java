40926073820f4826b89ebbff4dc741f170bb42f0
Alvaro <Alvaro@192.168.15.23>
2017-01-21 18:52:23 -0200

    cadastro de usuarios

--- Agenda/src/br/indra/alvaro/Usuarios.java
+++ Agenda/src/br/indra/alvaro/Usuarios.java
@@ -1,6 +1,25 @@
 package br.indra.alvaro;
 
 public class Usuarios {
-	
+	private String nome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	private String  endereco;
 
+
+
+public void cadastrar(String nome, String endereco){
+	System.out.print("cadastrado com sucesso");
+	
+}
 }
